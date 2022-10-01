a383d5299e23350bd892e18f17fc3b3e51d71335
violletta1 <villygeorgieva@yahoo.com>
2022-10-01 19:39:41 +0100

    Update README.md

---
+++ Lists Basics/Exersice/invert_values.py
@@ -0,0 +1,13 @@
+single_string = input().split()
+opposite_numbers = []
+for element in single_string:
+    current_number = -int(element)
+    opposite_numbers.append(current_number)
+print(opposite_numbers)
+
+# list_of_numbers = input().split()
+# opposite_numbers = []
+# for element in list_of_numbers:
+#     current_number = -int(element)
+#     opposite_numbers.append(current_number)
+# print(opposite_numbers)
