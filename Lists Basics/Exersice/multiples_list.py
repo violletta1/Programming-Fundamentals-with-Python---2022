f8529ac7331d17df444111d37b3ec40645eecd1a
violletta1 <villygeorgieva@yahoo.com>
2022-10-02 17:31:36 +0100

    Update README.md

--- Lists Basics/Exersice/multiples_list.py
+++ Lists Basics/Exersice/multiples_list.py
@@ -3,9 +3,17 @@
 # which are multiples of the given factor. The numbers should be only positive,
 # and they should be arranged in ascending order, starting from the value of the factor.
 
+# factor = int(input())
+# count = int(input())
+# list_of_numbers = []
+# for multiplier in range(1, count + 1):
+#     list_of_numbers.append(factor * multiplier)
+# print(list_of_numbers)
+
 factor = int(input())
 count = int(input())
-list_of_numbers = []
-for multiplier in range(1, count + 1):
-    list_of_numbers.append(factor * multiplier)
-    print(list_of_numbers)
+my_list = []
+for num in range(1, count + 1):
+    calculation = num * factor
+    my_list.append(calculation)
+print(my_list)
