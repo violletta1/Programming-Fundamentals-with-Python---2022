aab87339bc806ae0c2a63ca069b2a73bcb01af00
violletta1 <villygeorgieva@yahoo.com>
2022-10-05 21:12:08 +0100

    Update README.md

--- Lists Basics/Exersice/survival_of_the_biggest.py
+++ Lists Basics/Exersice/survival_of_the_biggest.py
@@ -1,8 +1,28 @@
+#
+numbers = input().split(" ")
+count_remove_num = int(input())
+# print([int(x) for x in numbers])
 
-numbers = input().split()
-count_of_numbers_to_remove = int(input())
+dig_num = [int(x) for x in numbers]
+# print(dig_num)
 
-numbers.sort()
+# del dig_num[0: count_remove_num]
+#
+# print(dig_num)
+
+for current_remove in range(count_remove_num):
+    smallest_num = min(dig_num)
+    dig_num.remove(smallest_num)
+#     # print(dig_num)
+# print(dig_num)
 
 
-print(numbers)
+string_ints = [str(num) for num in dig_num]
+# Convert each integer to a string
+
+
+str_of_ints = ", ".join(string_ints)
+# Combine each string with a comma
+
+
+print(str_of_ints)
