e029caf83e418c1ea1cf6000ce00ffa0d4b9e579
violletta1 <villygeorgieva@yahoo.com>
2022-10-10 11:21:07 +0100

    Update README.md

---
+++ Lists Basics/Exersice/hello_france.py
@@ -0,0 +1,42 @@
+collection = input()
+budget = float(input())
+
+collection_list = collection.split("|")
+
+clothes_price = 50.00
+shoes_price = 35.00
+accessories_price = 20.50
+saved_sum = 0
+profit_sum = 0
+
+list_buy = []
+
+for x in collection_list:
+    purchase = x.split("->")
+    item = purchase[0]
+    price = float(purchase[1])
+
+    if (item == "Clothes" and price <= clothes_price and price <= budget) or (
+            item == "Shoes" and price <= shoes_price and price <= budget) or \
+            (item == "Accessories" and price <= shoes_price and price <= budget):
+        budget -= price
+        profit_price = price * 1.4
+        saved_sum += profit_price
+        list_buy.append(profit_price)
+        profit_sum += profit_price - price
+    else:
+        continue
+
+saved_sum = saved_sum + budget
+
+for x in list_buy:
+    print(f"{x:.2f}", end=" ")
+print()
+print(f"Profit: {profit_sum:.2f}")
+if saved_sum >= 150:
+    print("Hello, France!")
+else:
+    print("Time to go.")
+    profit_price = price * 1.4
+    и
+    profit_sum += profit_price - price
--- Lists Basics/Exersice/multiples_list.py
+++ Lists Basics/Exersice/multiples_list.py
@@ -17,3 +17,17 @@ for num in range(1, count + 1):
     calculation = num * factor
     my_list.append(calculation)
 print(my_list)
+#
+# n = int(input())
+# word = input()
+# my_list = []
+# for _ in range(n):
+#     current_word = input()
+#     my_list.append(current_word)
+# print(my_list)
+#
+# for i in range(len(my_list) - 1, -1, -1):
+#     element = my_list[i]
+#     if word not in element:
+#         my_list.remove(element)
+# print(my_list)
